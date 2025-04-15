2a07cbb83646e9e0f2705c227632f132453db44f
Areeba155 <areebasaif144@gmail.com>
2025-04-15 16:01:51 +0500

    done

---
+++ python1/newfile.py
@@ -0,0 +1,21 @@
+a = 87463
+b = 2
+result = a**b
+print (result)
+c = 51
+d = 5
+print (c+d)
+print (c-d)
+print (c*d)
+print (c/d)
+print (c%d)
+print (c**d)
+print (c//d)
+thislist = ["apple", "banana", "cherry"]
+print(thislist)
+std = "ali" , "alisa"
+print (std)
+
+g = ["akram","iza"]
+g.append("saman")
+print (g)
---
+++ python1/python1.py
@@ -0,0 +1,24 @@
+a = 20
+print(a)
+b = 30
+print(b)
+a + b 
+print(a+b)
+print(a,b)
+num = 30
+print(num, type(num))
+name = "areeba"
+print(name, type(name) )
+
+num1 = 30 
+num2 = 40
+print( num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+print(num1 ** num2)
+print(num1 % num2)
+print(num1 // num2)
+num1 = 30
+num2 = 10
+print(num1 ** num2)
